e0597495b461359b554459a208a03faf3ad1d927
Glauco Telino <glaucolocao@gmail.com>
2025-11-22 23:38:44 -0300

    Initial commit

---
+++ app/src/main/java/com/glauco/weatherapp/WeatherApp.kt
@@ -0,0 +1,31 @@
+package com.glauco.weatherapp
+
+import android.app.Application
+import android.content.Intent
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class WeatherApp : Application() {
+    private val flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or
+            Intent.FLAG_ACTIVITY_NEW_TASK or
+            Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+    override fun onCreate() {
+        super.onCreate()
+        Firebase.auth.addAuthStateListener { firebaseAuth ->
+            if (firebaseAuth.currentUser != null) {
+                goToMain()
+            } else {
+                goToLogin()
+            }
+        }
+    }
+
+    private fun goToMain() {
+        this.startActivity(Intent(this, MainActivity::class.java).setFlags(flags))
+    }
+
+    private fun goToLogin() {
+        this.startActivity(Intent(this, LoginActivity::class.java).setFlags(flags))
+    }
+}
---
+++ app/src/main/java/com/glauco/weatherapp/viewmodel/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.glauco.weatherapp.viewmodel
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+import com.glauco.weatherapp.model.City
+import com.google.android.gms.maps.model.LatLng
+
+class MainViewModel : ViewModel() {
+    private val _cities = getInitialCities().toMutableStateList()
+
+    val cities: List<City>
+        get() = _cities
+
+    fun remove(city: City) {
+        _cities.remove(city)
+    }
+
+    fun add(name: String, location: LatLng? = null) {
+        _cities.add(City(name = name, location = location))
+    }
+
+    private fun getInitialCities() = List(20) { i ->
+        City(name = "Cidade $i", weather = "Carregando clima...")
+    }
+}
